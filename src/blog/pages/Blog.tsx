06ce7f1307f789ac84ad5cb98da3587b10119f62
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-21 18:50:15 -0300

    Creating blog page

---
+++ src/blog/pages/Blog.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NextPage } from "next";
+import { Box, Typography } from "@mui/material";
+
+const Blog: NextPage = () => {
+  return (
+    <Box>
+      <Typography>TESTE</Typography>
+    </Box>
+  );
+};
+
+export default Blog;

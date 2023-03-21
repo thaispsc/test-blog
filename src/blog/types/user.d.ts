b504d6411f03ba748837076b1efd604de03c662f
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-21 20:01:26 -0300

    Describing object properties using interface

---
+++ src/blog/types/user.d.ts
@@ -0,0 +1,28 @@
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  address: UserAddress;
+  phone: string;
+  website: string;
+  company: UserCompany;
+}
+
+interface UserAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: UserGeo;
+}
+
+interface UserGeo {
+  lat: string;
+  lng: string;
+}
+
+interface UserCompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}

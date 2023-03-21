12da6174e83b1b29352da57694b139d8e00bf001
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-21 20:52:39 -0300

    Defining ServerSideProps to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
 import Blog from "@/blog/pages/Blog";
+import { getPosts } from "@/blog/services/posts";
+import NextError from "@/lib/components/Error";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  try {
+    const posts = await getPosts();
+    return {
+      props: {
+        error: null,
+        posts,
+      },
+    };
+  } catch (e) {
+    const error = e as Error;
+    return {
+      props: {
+        error,
+        posts: null,
+      },
+    };
+  }
+};
+
+type PageProps = {
+  error: Error | null;
+  posts: Post[] | null;
+};
+
+export default function Home({ error, posts }: PageProps) {
   return (
     <>
-      <Blog />
+      {error && <NextError message={error.message} />}
+      {posts && <Blog posts={posts} />}
     </>
   );
 }

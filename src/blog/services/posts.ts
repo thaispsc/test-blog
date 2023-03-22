fc79a416e14002015798759c6b2835dbc285aa18
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-22 00:16:05 -0300

    getPostById function

--- src/blog/services/posts.ts
+++ src/blog/services/posts.ts
@@ -9,3 +9,13 @@ export const getPosts = async () => {
     throw err;
   }
 };
+
+export const getPostById = async (id: number) => {
+  try {
+    const { data } = await api.get(`/posts/${id}`);
+    return data;
+  } catch (error) {
+    const err = new Error("Failed to get post");
+    throw err;
+  }
+};

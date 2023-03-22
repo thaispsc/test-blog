798e69c8a4a56f0f708d79ff38c166138239f3b2
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-22 20:22:09 -0300

    Conditional render

--- src/blog/pages/Post.tsx
+++ src/blog/pages/Post.tsx
@@ -23,28 +23,34 @@ const Post: NextPage<PostProps> = ({ post, comments }) => {
   return (
     <>
       <Header />
-      <Container maxWidth="md">
-        <Typography variant="h1" marginTop={10} align="center">
-          {post.title}
-        </Typography>
-        <Typography variant="h2" align="center">
-          {user.name}
-        </Typography>
-        <Box height={500} marginY={2} sx={{ backgroundColor: "#bababa" }}></Box>
-        <Typography>{post.body}</Typography>
-        <Box marginTop={5}>
-          <Typography variant="h2">Comments</Typography>
-          {comments.map((comment) => (
-            <Card sx={{ marginY: "16px" }}>
-              <CardContent>
-                <Typography>{comment.name}</Typography>
-                <Typography>{comment.email}</Typography>
-                <Typography>{comment.body}</Typography>
-              </CardContent>
-            </Card>
-          ))}
-        </Box>
-      </Container>
+      {Object.keys(user).length !== 0 && (
+        <Container maxWidth="md">
+          <Typography variant="h1" marginTop={8} align="center">
+            {post.title}
+          </Typography>
+          <Typography variant="h2" align="center">
+            {user.name}
+          </Typography>
+          <Box
+            height={500}
+            marginY={2}
+            sx={{ backgroundColor: "#bababa" }}
+          ></Box>
+          <Typography>{post.body}</Typography>
+          <Box marginTop={5}>
+            <Typography variant="h2">Comments</Typography>
+            {comments.map((comment) => (
+              <Card sx={{ marginY: "16px" }}>
+                <CardContent>
+                  <Typography>{comment.name}</Typography>
+                  <Typography>{comment.email}</Typography>
+                  <Typography>{comment.body}</Typography>
+                </CardContent>
+              </Card>
+            ))}
+          </Box>
+        </Container>
+      )}
     </>
   );
 };

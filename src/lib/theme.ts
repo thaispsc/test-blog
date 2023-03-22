ddb9899d286a19b462ea54987c2367f43e0c66a1
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-22 00:48:07 -0300

    Creating theme

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,5 +1,21 @@
 import { createTheme } from "@mui/material";
 
-const theme = createTheme({});
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#000000",
+    },
+    primary: {
+      main: "#ffffff",
+    },
+    secondary: {
+      main: "#000000",
+    },
+  },
+  typography: {
+    h1: { fontSize: "2rem" },
+    body1: { fontSize: "1rem" },
+  },
+});
 
 export default theme;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import theme from "@/lib/theme";
-import { ThemeProvider } from "@mui/material";
+import { ThemeProvider, GlobalStyles } from "@mui/material";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles
+        styles={{
+          body: {
+            margin: 0,
+          },
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );

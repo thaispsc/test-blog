70ee1383c32ea693ada32747fca49e553b47d901
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-23 15:29:01 -0300

    Changing Post Page ui

--- src/blog/pages/Post.tsx
+++ src/blog/pages/Post.tsx
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { NextPage } from "next";
-import { Box, Container, Typography, Card, CardContent } from "@mui/material";
+import {
+  Box,
+  Container,
+  Typography,
+  Card,
+  CardContent,
+  Link,
+  Grid,
+  useMediaQuery,
+  Theme,
+} from "@mui/material";
 import Header from "../ui/header/Header";
 import { getUserById } from "../services/users";
+import UserIcon from "@/lib/components/UserIcon";
 
 interface PostProps {
   post: Post;
@@ -20,19 +31,36 @@ const Post: NextPage<PostProps> = ({ post, comments }) => {
     fetchUser();
   }, []);
 
+  const isUserFetched = Object.keys(user).length !== 0;
+
+  const isMobile = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down("sm")
+  );
+
   return (
     <>
       <Header />
-      {Object.keys(user).length !== 0 && (
+      {isUserFetched && (
         <Container maxWidth="md">
           <Typography variant="h1" marginTop={8} align="center">
             {post.title}
           </Typography>
-          <Typography variant="h2" align="center">
-            {user.name}
-          </Typography>
+          <Link
+            href={`/users/${user.id}`}
+            underline="none"
+            color="secondary"
+            sx={{
+              "&:hover": {
+                color: "#838383",
+              },
+            }}
+          >
+            <Typography variant="h2" align="center" marginTop={2}>
+              {user.name}
+            </Typography>
+          </Link>
           <Box
-            height={500}
+            paddingTop="50%"
             marginY={2}
             sx={{ backgroundColor: "#bababa" }}
           ></Box>
@@ -40,12 +68,37 @@ const Post: NextPage<PostProps> = ({ post, comments }) => {
           <Box marginTop={5}>
             <Typography variant="h2">Comments</Typography>
             {comments.map((comment) => (
-              <Card sx={{ marginY: "16px" }}>
-                <CardContent>
-                  <Typography>{comment.name}</Typography>
-                  <Typography>{comment.email}</Typography>
-                  <Typography>{comment.body}</Typography>
-                </CardContent>
+              <Card
+                variant="outlined"
+                sx={{ marginY: "16px", backgroundColor: "#EDEDED" }}
+              >
+                <Grid
+                  container
+                  paddingX={isMobile ? 0 : 4}
+                  display="flex"
+                  alignItems="center"
+                >
+                  <Grid item xs={2} display={isMobile ? "none" : "auto"}>
+                    <UserIcon size={90} />
+                  </Grid>
+                  <Grid item xs={isMobile ? 12 : 10}>
+                    <CardContent>
+                      <Box display={isMobile ? "auto" : "flex"}>
+                        <Typography paddingRight={isMobile ? 0 : 2}>
+                          {comment.name}
+                        </Typography>
+                        <Typography
+                          paddingLeft={isMobile ? 0 : 2}
+                          borderLeft={isMobile ? 0 : 2}
+                          borderColor="#bababa"
+                        >
+                          {comment.email}
+                        </Typography>
+                      </Box>
+                      <Typography>{comment.body}</Typography>
+                    </CardContent>
+                  </Grid>
+                </Grid>
               </Card>
             ))}
           </Box>

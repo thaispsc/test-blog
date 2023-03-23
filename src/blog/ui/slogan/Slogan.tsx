16016ecf22082ea7283c4130f0da3ff9271c962a
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-23 19:37:23 -0300

    Creating Slogan component

---
+++ src/blog/ui/slogan/Slogan.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const Slogan = () => {
+  return (
+    <Box textAlign="center" marginY={8}>
+      <Typography fontSize="3rem">Blog,</Typography>
+      <Typography variant="h1">Lorem, ipsum dolor</Typography>
+      <Typography variant="h1">consectetur adipisicing.</Typography>
+    </Box>
+  );
+};
+
+export default Slogan;

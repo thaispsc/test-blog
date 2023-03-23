9c584bb9f544772d6d3ef199727feddfad6428c2
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-23 20:53:52 -0300

    Creating Blog page

--- src/blog/pages/Blog.tsx
+++ src/blog/pages/Blog.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 import { NextPage } from "next";
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
+import Header from "../ui/header/Header";
+import UserIcon from "@/lib/components/UserIcon";
+import Slogan from "../ui/slogan/Slogan";
+import MeetUsers from "../ui/meet-users-section/MeetUsers";
+import FeaturedPosts from "../ui/featured-posts/FeaturedPosts";
 
-const Blog: NextPage = () => {
+interface BlogProps {
+  posts: Post[];
+  users: User[];
+}
+
+const Blog: NextPage<BlogProps> = ({ posts, users }) => {
   return (
-    <Box>
-      <Typography>TESTE</Typography>
-    </Box>
+    <>
+      <Header />
+      <Container maxWidth="md">
+        <Slogan />
+      </Container>
+      <Box paddingY={8} sx={{ backgroundColor: "#EDEDED" }}>
+        <MeetUsers users={users} />
+      </Box>
+      <Container maxWidth="md">
+        <FeaturedPosts posts={posts} />
+      </Container>
+    </>
   );
 };
 

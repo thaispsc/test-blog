94f3adde2cb576a13ad74e42cccc7ef473b4892e
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-24 10:58:00 -0300

    Creating UserData component

--- src/blog/pages/User.tsx
+++ src/blog/pages/User.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { NextPage } from "next";
-import { Box, Container, Typography, Grid, useMediaQuery } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import Header from "../ui/header/Header";
 import UserIcon from "../../lib/components/UserIcon";
-import { Theme } from "@mui/system";
+import UserData from "../ui/user-data/UserData";
 
 interface UserProps {
   user: User;
 }
 
 const User: NextPage<UserProps> = ({ user }) => {
-  const isMobile = useMediaQuery((theme: Theme) =>
-    theme.breakpoints.down("sm")
-  );
   return (
     <>
       <Header />
@@ -23,95 +20,7 @@ const User: NextPage<UserProps> = ({ user }) => {
             {user.name}
           </Typography>
         </Box>
-        <Grid
-          container
-          display="flex"
-          justifyContent="space-between"
-          marginTop={6}
-        >
-          <Grid item xs={isMobile ? 12 : 4}>
-            <Box>
-              <Typography variant="h2" marginBottom={2}>
-                Basic Informations
-              </Typography>
-              <Typography>
-                <span style={{ fontWeight: "bold" }}>Username: </span>
-                {user.username}
-              </Typography>
-              <Typography>
-                <span style={{ fontWeight: "bold" }}>Website: </span>
-                {user.website}
-              </Typography>
-            </Box>
-            <Box marginTop={2}>
-              <Typography variant="h2" marginBottom={2}>
-                Contact
-              </Typography>
-              <Typography>
-                <span style={{ fontWeight: "bold" }}>Email: </span>
-                {user.email}
-              </Typography>
-              <Typography>
-                <span style={{ fontWeight: "bold" }}>Phone: </span>
-                {user.phone}
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid
-            item
-            xs={isMobile ? 12 : 4}
-            display="flex"
-            justifyContent={isMobile ? "flex-start" : "center"}
-            marginY={isMobile ? 2 : 0}
-          >
-            <Box>
-              <Typography variant="h2" marginBottom={2}>
-                Address
-              </Typography>
-              <Typography>
-                <span style={{ fontWeight: "bold" }}>Street: </span>
-                {user.address.street}
-              </Typography>
-              <Typography>
-                <span style={{ fontWeight: "bold" }}>Suite: </span>
-                {user.address.suite}
-              </Typography>
-              <Typography>
-                <span style={{ fontWeight: "bold" }}>City: </span>
-                {user.address.city}
-              </Typography>
-              <Typography>
-                <span style={{ fontWeight: "bold" }}>Zipcode: </span>
-                {user.address.zipcode}
-              </Typography>
-              <Typography>
-                <span style={{ fontWeight: "bold" }}>Latitude: </span>
-                {user.address.geo.lat}
-              </Typography>
-              <Typography>
-                <span style={{ fontWeight: "bold" }}>Longitude: </span>
-                {user.address.geo.lng}
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={isMobile ? 12 : 4}>
-            <Typography variant="h2" marginBottom={2}>
-              Company
-            </Typography>
-            <Typography>
-              <span style={{ fontWeight: "bold" }}>Name: </span>
-              {user.company.name}
-            </Typography>
-            <Typography>
-              <span style={{ fontWeight: "bold" }}>Catch Phrase: </span>
-              {user.company.catchPhrase}
-            </Typography>
-            <Typography>
-              <span style={{ fontWeight: "bold" }}>BS: </span>
-              {user.company.bs}
-            </Typography>
-          </Grid>
-        </Grid>
+        <UserData user={user} />
       </Container>
     </>
   );
---
+++ src/blog/ui/user-data/UserData.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { Grid, Box, Typography, Theme, useMediaQuery } from "@mui/material";
+
+interface UserDataProps {
+  user: User;
+}
+
+export const UserData = ({ user }: UserDataProps) => {
+  const isMobile = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down("sm")
+  );
+
+  return (
+    <Grid container display="flex" justifyContent="space-between" marginTop={6}>
+      <Grid item xs={isMobile ? 12 : 4}>
+        <Box>
+          <Typography variant="h2" marginBottom={2}>
+            Basic Informations
+          </Typography>
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Username: </span>
+            {user.username}
+          </Typography>
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Website: </span>
+            {user.website}
+          </Typography>
+        </Box>
+        <Box marginTop={2}>
+          <Typography variant="h2" marginBottom={2}>
+            Contact
+          </Typography>
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Email: </span>
+            {user.email}
+          </Typography>
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Phone: </span>
+            {user.phone}
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid
+        item
+        xs={isMobile ? 12 : 4}
+        display="flex"
+        justifyContent={isMobile ? "flex-start" : "center"}
+        marginY={isMobile ? 2 : 0}
+      >
+        <Box>
+          <Typography variant="h2" marginBottom={2}>
+            Address
+          </Typography>
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Street: </span>
+            {user.address.street}
+          </Typography>
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Suite: </span>
+            {user.address.suite}
+          </Typography>
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>City: </span>
+            {user.address.city}
+          </Typography>
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Zipcode: </span>
+            {user.address.zipcode}
+          </Typography>
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Latitude: </span>
+            {user.address.geo.lat}
+          </Typography>
+          <Typography>
+            <span style={{ fontWeight: "bold" }}>Longitude: </span>
+            {user.address.geo.lng}
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={isMobile ? 12 : 4}>
+        <Typography variant="h2" marginBottom={2}>
+          Company
+        </Typography>
+        <Typography>
+          <span style={{ fontWeight: "bold" }}>Name: </span>
+          {user.company.name}
+        </Typography>
+        <Typography>
+          <span style={{ fontWeight: "bold" }}>Catch Phrase: </span>
+          {user.company.catchPhrase}
+        </Typography>
+        <Typography>
+          <span style={{ fontWeight: "bold" }}>BS: </span>
+          {user.company.bs}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default UserData;

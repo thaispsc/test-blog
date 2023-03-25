c45121702c5d360614568f266affb4a34bf27b0d
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-03-25 17:49:27 -0300

    Ui Error component

--- src/lib/components/Error.tsx
+++ src/lib/components/Error.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Container, Typography, Box } from "@mui/material";
 
 export default function Error({ message }: { message?: string }) {
   return (
-    <div>
-      <p>Something bad happened.</p>
-      {message && <p>{message}</p>}
-    </div>
+    <Container>
+      <Box marginTop={8}>
+        <Typography variant="h1" textAlign="center" marginBottom={2}>
+          Something bad happened.
+        </Typography>
+        {message && (
+          <Typography variant="h2" textAlign="center">
+            {message}
+          </Typography>
+        )}
+      </Box>
+    </Container>
   );
 }
